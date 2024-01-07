c466cb1b7d90df90e2aceb67723d904045463229
soniya <soniyakaramchandani866@gmail.com>
2024-01-07 11:25:12 +0530

    socketio changes

--- backend/server.js
+++ backend/server.js
@@ -75,9 +75,9 @@ io.on("connection", (socket) => {
   socket.on("new message", (newMessageRecieved) => {
     var chat = newMessageRecieved.chat;
 
-    if (!chat.users) return console.log("chat.users not defined");
+    // if (!chat.users) return console.log("chat.users not defined");
 
-    chat.users.forEach((user) => {
+    chat?.users.forEach((user) => {
       if (user._id == newMessageRecieved.sender._id) return;
 
       socket.in(user._id).emit("message recieved", newMessageRecieved);
