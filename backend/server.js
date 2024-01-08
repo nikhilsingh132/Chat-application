68d82555399342fef62b6fe0226ce79dd79c8df8
nikhilsingh132 <nikhil.kumarsingh.cd.ece20@itbhu.ac.in>
2024-01-08 18:58:51 +0530

    cors changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const {chats}=require('./data/data')
 dotenv.config();
 connectDB();
 const app = express();
-const server=app.listen(5000,console.log('server started'))
+const port = process.env.PORT || 5000;
+const server=app.listen(port,console.log('server started'))
+const cors = require("cors")
+
+//enable cors
+app.use(cors());
 
 app.use(express.json()); // to accept json data
  app.get("/", (req, res) => {
